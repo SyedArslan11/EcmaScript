1f2615114efee0bae807342c7afcfe5f6b6b9ca4
SyedArslan11 <syedarslan007@gmail.com>
2021-07-15 16:54:01 +0530

    7/15/2021
    math function
    min/max/round/floor/ceil

--- Amount.js
+++ Amount.js
@@ -1,4 +1,7 @@
 const AvgCost = (distance, Mileage, FuelPrice) =>
-    (distance/Mileage)* FuelPrice;
+   Math.round((distance/Mileage)* FuelPrice);
 
     console.log(`Amount need to fill fuel is Rs. ${AvgCost(303,37,110.08)}`);
+
+
+    console.log(Math.ceil(12.44));
--- difference.js
+++ difference.js
@@ -1,3 +1,3 @@
-const diffrence = (num1, num2) => num1 - num2;
+const diffrence = (num1, num2) =>Math.abs(num1 - num2);
 
 console.log(`Age diffrence is ${diffrence(25,45)} Years`);
